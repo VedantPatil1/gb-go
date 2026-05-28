ab53e606d431ddcf352b884438befee7a72eb232
Vedant Patil <vedantpatil.w.1@gmail.com>
2026-05-28 14:28:06 +0530

    feat(db): implement account schema and generate sqlc code

---
+++ db/queries.sql
@@ -0,0 +1,26 @@
+-- name: GetAccount :one
+SELECT id, external_id, name, type, current_balance, last_updated, registered_on
+FROM accounts
+WHERE id = ?;
+
+-- name: CreateAccount :one
+INSERT INTO accounts (id, external_id, name, type, current_balance)
+VALUES (?, ?, ?, ?, ?) RETURNING *;
+
+-- name: UpdateAccount :one
+UPDATE accounts
+SET name = ?,
+    type = ?,
+    last_updated = CURRENT_TIMESTAMP
+WHERE id = ? RETURNING *;
+
+-- name: UpdateCurrentBalance :one
+UPDATE accounts
+SET current_balance = ?,
+    last_updated = CURRENT_TIMESTAMP
+WHERE id = ? RETURNING *;
+
+-- name: ListAccounts :many
+SELECT id, external_id, name, type, current_balance, last_updated, registered_on
+FROM accounts
+ORDER BY name;
---
+++ db/schema.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS accounts (
+    id TEXT PRIMARY KEY,
+    external_id TEXT UNIQUE,
+    name TEXT NOT NULL UNIQUE,
+    type TEXT NOT NULL,                   -- Treat purely as a TEXT primitive
+    current_balance REAL NOT NULL DEFAULT 0.0,
+    last_updated DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    registered_on DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
